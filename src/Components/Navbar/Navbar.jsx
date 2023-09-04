f24baf0a25604f2697815ac4ff659d72cd5f1809
Felype Henrique <felypehenrique25@gmail.com>
2023-09-04 12:15:38 -0300

    menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,28 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div className="navbar"></div>
+        <div className="header">
+            <div className="logoDiv">
+                <img src="" alt="" className="logo" />
+            </div>
+
+            <div className="navbar">
+                <ul className="menu">
+                    <li className="listItem">
+                        <a href="/" className="link">Carros Usados</a>
+                    </li>
+                    <li className="listItem">
+                        <a href="/" className="link">Carros Novos</a>
+                    </li>
+                    <li className="listItem">
+                        <a href="/" className="link">Leilões</a>
+                    </li>
+                    <li className="listItem">
+                        <a href="/" className="link">Vender</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
